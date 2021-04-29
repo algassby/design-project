6e19f350201bb7c03e231f352af59b064d30dd9a
algassby <algassby@hotmail.com>
2021-04-29 17:03:29 +0200

    right content

--- files/Mastère-1/Intégration-web/design-project/src/app/menu/menu.component.ts
+++ files/Mastère-1/Intégration-web/design-project/src/app/menu/menu.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  isMenuCollapsed = true;
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+  }
+  toggleMenu(): void{
+    this.isMenuCollapsed = !this.isMenuCollapsed;
   }
 
 }
